f275dcbaad1601144bb8831004b3340717d50951
Clark Mitchell <clark_mitchelljr@hotmail.com>
2022-11-07 21:22:19 -0700

    update

--- routes/books.js
+++ routes/books.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const Author = require("../models/author");
-const Book = require("../models/book");
+const Book = require("../models/books");
 const router = express.Router();
+const path = require('path')
+const uploadPath = path.join('public', Book.coverImageBasePath)
+const multer = require('multer')
+const imageMimeTypes = ['images/jpeg', 'images/png', 'images/gif']
+const upload = multer({
+    dest: uploadPath,
+    fileFilter:(req, file, callback) => {
+        callback(null, imageMimeTypes.includes(file.mimetype))
+    }
+})
 
 //All Books Route
 router.get("/", async (req, res) => {
@@ -23,8 +33,16 @@ router.get("/new", async (req, res) => {
 });
 
 //Create Book Route
-router.post("/", async (req, res) => {
-    res.send('Create Book')
+router.post("/", upload.single('cover'), async (req, res) => {
+    const book = new Book({
+        title: req.body.title,
+        author: req.body.author,
+        publishDate: new Date(req.body.publishDate),
+        pageCount: req.body.pageCount,
+        description: req.body.description
+    })
 });
+
+
 
 module.exports = router;
