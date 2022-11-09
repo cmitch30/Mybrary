de461e808a056b6c07dbc82bf610e5895409441e
Clark Mitchell <clark_mitchelljr@hotmail.com>
2022-11-08 23:31:42 -0700

    fixed

--- routes/authors.js
+++ routes/authors.js
@@ -30,10 +30,10 @@ router.post("/", async (req, res) => {
     const newAuthor = await author.save()
     //  res.redirect(`authors/${newAuthor.id}`)
         res.redirect(`authors`);
-  } catch (error) {
+  } catch {
     res.render("authors/new", {
       author: author,
-      errorMessage: "Error creating Author",
+      errorMessage: "Error creating Author"
     });
   }
 });
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,4 @@ router.get ('/', (req, res) => {
 })
 
 
-module.exports = router
+module.exports = router;
